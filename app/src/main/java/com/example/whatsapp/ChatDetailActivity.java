1012df81f0541d8e92837c837a7d551b7e86a872
anthienduong <anthienduong2000@gmail.com>
2021-06-15 20:07:50 +0700

    first commit

---
+++ app/src/main/java/com/example/whatsapp/ChatDetailActivity.java
@@ -0,0 +1,120 @@
+package com.example.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.whatsapp.Adapters.ChatAdapter;
+import com.example.whatsapp.Models.MessageModel;
+import com.example.whatsapp.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        final String senderID = auth.getUid();
+        String receiverId = getIntent().getStringExtra("userID");
+        String userName = getIntent().getStringExtra("userName");
+        String profilePic = getIntent().getStringExtra("profilePic");
+
+        binding.userName.setText(userName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.ic_user__1_).into(binding.profileImage);
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels, this);
+        binding.chatRecylearView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecylearView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderID + receiverId;
+        final String receiveRoom = receiverId + senderID;
+
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messageModels.clear();
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                            MessageModel messageModel = snapshot1.getValue(MessageModel.class);
+                            messageModels.add(messageModel);
+                        }
+                        chatAdapter.notifyDataSetChanged();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = binding.etMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderID, message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText("");
+
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .push()
+                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        database.getReference().child("chats")
+                                .child(receiveRoom)
+                                .push()
+                                .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
